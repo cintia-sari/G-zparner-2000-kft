2ec7cb7b264bc3c282c1df019f1043c0ec1bbd2e
Sári Cintia <sari.cintia1996@gmail.com>
2023-03-17 14:38:30 +0100

    setting basic galery

--- képekDiv.js
+++ képekDiv.js
@@ -1,7 +1,28 @@
-document.querySelector("#képek-div");
+const leftBtn = document.querySelector(".picture-button-left-js");
+const rightBtn = document.querySelector(".picture-button-right-js")
+const pictureDiv = document.querySelector(".képek-div-js");
+let pictureIndex= 50;
 
 // képeket tartalmazó mappa
 
+const galery = [];
+
+for (let i =0 ; i < 122; i++){
+     galery.push(`<img class="galery-img" src="./src/galery/${i}.jpg">`)
+}
 
 
 
+leftBtn.addEventListener("click", function(){
+    console.log("left")
+    pictureIndex= pictureIndex-1;
+    pictureDiv.innerHTML = galery[pictureIndex];
+})
+
+rightBtn.addEventListener("click", function(){
+    pictureIndex= pictureIndex+1;
+    pictureDiv.innerHTML = galery[pictureIndex];
+})
+
+console.log(pictureIndex)
+pictureDiv.innerHTML = galery[pictureIndex];
