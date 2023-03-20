6c86caca3e25ff33c62a5f0529995d35d3e865d8
Sári Cintia <sari.cintia1996@gmail.com>
2023-03-20 14:51:29 +0100

    editing hero section

--- src/scroll.js
+++ src/scroll.js
@@ -2,27 +2,30 @@ const proba = document.querySelector('.js-menüText');
 const banner = document.querySelector('.js-banner');
 const button = document.querySelector('.js-button');
 
+//banner.style.backgroundSize ="100vw";
+//banner.style.backgroundSize ="150vw";
+
 const onScroll = (event) =>{
     const scrollPosition = 
         event.target.scrollingElement.scrollTop;
 
-    if(scrollPosition > 170 ){
-        banner.style.backgroundSize ="150%";
-        proba.style.opacity = 0;
-        proba.style.translate = "0 -50px";
-        proba.style.scale = "0.9";
-        button.style.opacity = 0;
-        button.style.translate = "0 -40px";
-        button.style.scale = "0.8";
-    }else{
-        banner.style.backgroundSize ="100%";
-        proba.style.opacity = 1;
-        proba.style.translate = 0;
-        proba.style.scale = 1;
-        button.style.opacity = 1;
-        button.style.translate = 0;
-        button.style.scale = 1;
-    }
+        if(scrollPosition > 170 ){
+            banner.style.backgroundSize ="150vw";
+            proba.style.opacity = 0;
+            proba.style.translate = "0 -50px";
+            proba.style.scale = "0.9";
+            button.style.opacity = 0;
+            button.style.translate = "0 -40px";
+            button.style.scale = "0.8";
+        }else{
+            banner.style.backgroundSize ="180vw";
+            proba.style.opacity = 1;
+            proba.style.translate = 0;
+            proba.style.scale = 1;
+            button.style.opacity = 1;
+            button.style.translate = 0;
+            button.style.scale = 1;
+        }
 };
 
 document.addEventListener("scroll", onScroll);
